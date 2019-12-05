01192f81b877b16740df5158f1e087e752eb3bbc
Anuj Dutt <anuj.dutt9@gmail.com>
2019-12-05 08:19:03 -0500

    Add files via upload

---
+++ PyTorch-TorchScript/build/CMakeFiles/example-app.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/anujdutt/Downloads/Anuj/PyTorch-TorchScript/example-app.cpp" "/Users/anujdutt/Downloads/Anuj/PyTorch-TorchScript/build/CMakeFiles/example-app.dir/example-app.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AT_PARALLEL_OPENMP=1"
+  "_THP_CORE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libtorch/include"
+  "../libtorch/include/torch/csrc/api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
